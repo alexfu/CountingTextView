1799c9d8fca4ccf75ca18f51461188680aa7034a
Alex Fu <alex.fu914@gmail.com>
2015-10-15 10:19:38 -0400

    Convert project to a library with included sample

---
+++ sample/src/main/java/com/alexfu/sample/MainActivity.java
@@ -0,0 +1,36 @@
+package com.alexfu.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.alexfu.countingtextview.CountingTextSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    final CountingTextSwitcher countingTextSwitcher = (CountingTextSwitcher) findViewById(R.id.countingTextSwitcher);
+    Button increment = (Button) findViewById(R.id.increment);
+    Button increment2 = (Button) findViewById(R.id.increment2);
+
+    increment.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        countingTextSwitcher.increment();
+      }
+    });
+
+    increment2.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        countingTextSwitcher.reset();
+        countingTextSwitcher.incrementTo(850);
+      }
+    });
+  }
+}
